e763542d9a6261b1529327bbeda9ba674fb0dbce
Billy Sentana <billy.sentana@izeno.com>
2018-10-23 17:03:22 +0800

    properties change

--- target/classes/sql/initDependecy.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO master.feedback (id ,objective, title,type) values ('FEEDBACK09484',null,null,'PK');
-INSERT INTO master.feedback (id ,objective, title,type) values ('FEEDBACK44e52',null,null,'PKK');
-INSERT INTO master.feedback (id ,objective, title,type) values ('FEEDBACK46481',null,null,'PC');
-INSERT INTO master.feedback (id ,objective, title,type) values ('FEEDBACK89eef',null,null,'PBPC');
-INSERT INTO master.feedback (id ,objective, title,type) values ('FEEDBACK93da2',null,null,'PBCP');
